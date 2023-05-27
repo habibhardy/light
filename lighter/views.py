7c3be8ed6d1fe9e2252ba91df9a7859bb1f09057
Kouame Habib Hardy <habibhardy10@gmail.com>
2023-05-27 04:51:44 +0100

    first commit

---
+++ lighter/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
+# Create your views here.
+
+
+@csrf_exempt
+def helloworld(request):
+          return JsonResponse("Messege:Hello world :)" ,safe=False)
+                    
+          
