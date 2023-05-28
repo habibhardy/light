69ad00072a5e21d0699772a8b643a56a8e2c9b6e
Kouame Habib Hardy <habibhardy10@gmail.com>
2023-05-28 04:21:06 +0100

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 # build_files.sh
-py -m pip install -r requirements.txt
-py manage.py collectstatic --noinput --clear
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
